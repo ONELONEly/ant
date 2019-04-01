dea93a1c5f2900b3f8369f8fc6057affaa2eafcf
960814 <180484>
2019-04-01 21:15:57 +0800

    添加url访问

--- src/com/gree/ant/util/MyUtil.java
+++ src/com/gree/ant/util/MyUtil.java
@@ -1,5 +1,11 @@
 package com.gree.ant.util;
 
+import org.nutz.http.Header;
+import org.nutz.http.Http;
+import org.nutz.http.Response;
+import org.nutz.json.Json;
+import org.nutz.json.JsonFormat;
+
 import javax.servlet.ServletContext;
 
 public class MyUtil {
@@ -8,5 +14,15 @@ public class MyUtil {
 
     public String getPath(String path){
         return sc.getRealPath(path);
+    }
+
+    private String makePost(){
+        String url = "http://10.2.8.188/GreeBIWebapi/api/ticket"; //职层='中层干部'
+        String params1 = "{\"serverip\":\"10.2.4.88\",\"username\":\"erpuser\",\"password\":\"erp*951\",\"sitename\":\"ComputerBI\"}";
+        System.out.println(params1);
+        Map<String, Object> params2 = (Map<String, Object>) Json.fromJson(params1);
+        Header headers = Header.create().set("Content-Type", "application/json");
+        Response response = Http.post3(url, Json.toJson(params2, JsonFormat.compact()), headers, 200000, 2000);
+        return response.getContent();
     }
 }
