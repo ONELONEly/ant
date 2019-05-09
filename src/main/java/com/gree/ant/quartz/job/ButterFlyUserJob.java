6d13c206fe712c5d3ac1b81f50a62864775f170f
180484 <180484>
2019-05-09 17:19:47 +0800

    定时任务输出log4j日志

--- src/main/java/com/gree/ant/quartz/job/ButterFlyUserJob.java
+++ src/main/java/com/gree/ant/quartz/job/ButterFlyUserJob.java
@@ -3,14 +3,13 @@ package com.gree.ant.quartz.job;
 import com.gree.ant.dao.daoImp.ButterFlyDAOImp;
 import com.gree.ant.util.DateUtil;
 import com.gree.ant.util.SyncButterFlyData;
+import org.apache.log4j.Logger;
 import org.nutz.dao.Dao;
 import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 
@@ -23,13 +22,13 @@ public class ButterFlyUserJob implements Job {
     @Inject
     private ButterFlyDAOImp butterFlyDAOImp;
 
-    private Logger logger = LoggerFactory.getLogger(ButterFlyUserJob.class);
+    private Logger logger = Logger.getLogger(ButterFlyUserJob.class);
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        logger.info("执行同步金蝶用户开始，时间为{}", DateUtil.formatYMDHMSDate(new Date()));
+        logger.info("执行同步金蝶用户开始，时间为" +  DateUtil.formatYMDHMSDate(new Date()));
         SyncButterFlyData syncButterFlyData = SyncButterFlyData.createSyncDataUtil();
         syncButterFlyData.carrySync(dao,butterFlyDAOImp);
-        logger.info("执行同步金蝶用户结束，时间为{}",DateUtil.formatYMDHMSDate(new Date()));
+        logger.info("执行同步金蝶用户结束，时间为 " + DateUtil.formatYMDHMSDate(new Date()));
     }
 }
